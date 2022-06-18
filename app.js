b62a2b9581a0251e9631f98e91921ab0fa3fd4c2
Lucas Couto <loc.unb@gmai.com>
2022-06-18 02:15:33 -0300

    Pass user from form to list

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const path = require("path");
 
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const appRoutes = require("./routes/index");
 
 const app = express();
 
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use(appRoutes);
--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,19 @@
 const express = require("express");
 
 const router = express.Router();
+const users = [];
 
 router.get("/", (req, res) => {
   res.render("form", { pageTitle: "form" });
 });
 
 router.get("/users", (req, res) => {
-  res.render("users", { pageTitle: "Users" });
+  res.render("users", { pageTitle: "Users", users });
+});
+
+router.post("/add-user", (req, res) => {
+  users.push(req.body.username);
+  res.redirect("/users");
 });
 
 module.exports = router;
