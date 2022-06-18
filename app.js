626cac4ebdbfcafc3ecb9df3ced38ceda7cc17c2
Lucas Couto <loc.unb@gmai.com>
2022-06-18 01:37:42 -0300

    Includes initial ejs files

--- app.js
+++ app.js
@@ -3,5 +3,8 @@ const express = require("express");
 const appRoutes = require("./routes/index");
 
 const app = express();
+
+app.set("view engine", "ejs");
 app.use(appRoutes);
+
 app.listen(3000);
--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,12 @@ const express = require("express");
 
 const router = express.Router();
 
-router.get("/", (req, res) => {});
+router.get("/", (req, res) => {
+  res.render("form");
+});
 
-router.get("/users", (req, res) => {});
+router.get("/users", (req, res) => {
+  res.render("users");
+});
 
 module.exports = router;
